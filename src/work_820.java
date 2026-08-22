0a3273d3dcd9ee3804406b0615c9cb81d156793c
Yokok8 <1879458062@qq.com>
2026-08-22 11:42:35 +0800

    LeetCode 209.长度最小的子数组

--- src/work_820.java
+++ src/work_820.java
@@ -1,7 +1,7 @@
 public class work2 {
     public static void main(String[] args) {
         //8月20日
-        //长度最小的子数组
+        //LeetCode 209.长度最小的子数组
         //给定一个含有 n 个正整数的数组和一个正整数 target 。
         //找出该数组中满足其总和大于等于 target 的长度最小的 子数组 [numsl, numsl+1, ..., numsr-1, numsr]
         //并返回其长度。如果不存在符合条件的子数组，返回 0
