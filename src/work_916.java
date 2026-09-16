e8399f51b4543565d005d87c514a464a7fb09a69
Yokok8 <github>
2026-09-16 21:08:19 +0800

    LeetCode 334. 递增的三元子序列

---
+++ src/work_916.java
@@ -0,0 +1,34 @@
+public class work_916 {
+    public static void main(String[] args) {
+        //LeetCode 334. 递增的三元子序列
+        //给你一个整数数组 nums ，判断这个数组中是否存在长度为 3 的递增子序列。
+        //如果存在这样的三元组下标 (i, j, k) 且满足 i < j < k ，
+        //使得 nums[i] < nums[j] < nums[k] ，返回 true ；否则，返回 false 。
+
+        int[] nums = {2,1,5,0,4,6};
+
+        //枚举中间点	对每个 j，看「左边最小值 < nums[j] 且右边最大值 > nums[j]」
+        //固定中间那个数，左边找最小、右边找最大。
+
+        int n = nums.length;
+        int [] leftMin = new int[n];
+        int [] rightMin = new int[n];
+
+        leftMin[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            leftMin[i] = Math.min(leftMin[i-1], nums[i]);
+        }
+
+        rightMin[n-1] = nums[n-1];
+        for (int i = n-2; i >= 0; i--) {
+            rightMin[i] = Math.max(rightMin[i+1], nums[i]);
+        }
+
+        for (int i = 1; i < n-1; i++) {
+            if (leftMin[i-1] < nums[i] && nums[i] < rightMin[i+1]) {
+                System.out.println(true);
+            }
+        }
+
+    }
+}
